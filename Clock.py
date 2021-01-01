fa833189bcf452d37ff8979c9192e7a59a871a82
iashyam <70376025+iashyam@users.noreply.github.com>
2021-01-01 07:51:39 +0530

    Update Clock.py

--- Clock.py
+++ Clock.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 import time
 import math
 
@@ -8,6 +9,10 @@ class clock:
         self.window.title('Clock')
         self.lc = math.pi/30
         self.lc_hr = math.pi/6
+        self.lc_hrm = math.pi/(6*60)
+        self.text = "Happy NEw YEar"
+
+        self.img = ImageTk.PhotoImage(Image.open('clock.png'))
 
         self.show_time()
         self.move()
@@ -21,27 +26,22 @@ class clock:
             self.window.update()
 
     def show_time(self):
-        self.canvas = Canvas(self.window, height=500, width=500)
+        self.canvas = Canvas(self.window, height=500, width=500,bd=2)
         self.canvas.pack()
-        self.canvas.create_oval(60, 60, 440, 440, tags='circle', width=2)
-        self.canvas.create_line(self.polar(180, self.lc * self.second()) ,tags='second')
-        self.canvas.create_line(self.polar(150, self.lc_hr * self.hour()), width=4,arrow=LAST, tags = 'Hour')
-        self.canvas.create_line(self.polar(175, self.lc * self.miniute()), width=2,arrow=LAST, tags='minute')
-        for i in range(12):
-            if i%3 == 0:
-                self.canvas.create_line(self.polar3((self.lc_hr)*i) , width=4)
-            else:
-                self.canvas.create_line(self.polar3((self.lc_hr) * i))
-
+        self.canvas.create_image(250,250, image=self.img)
+        self.canvas.create_oval(246, 246, 254, 254, fill='black',tags='circle', width=2)
+        self.canvas.create_line(self.polar(100, self.lc * self.second()) ,tags='second',fill='green')
+        self.canvas.create_line(self.polar(70, (self.lc_hr * self.hour()+self.lc_hrm*self.miniute())), width=4,arrow=LAST, tags = 'Hour')
+        self.canvas.create_line(self.polar(95, self.lc * self.miniute()), width=2,arrow=LAST, tags='minute')
+        self.canvas.create_text(250, 90,text=self.text, font='times 20 bold')
+    
     def show_time2(self):
         self.canvas.destroy()
         self.show_time()
-
+        self.text=time.strftime('%H:%M:%S')
+        
     def polar(self, radius, theta):
         return 250, 250, (250 + radius* math.sin(theta)), (250- radius*math.cos(theta))
-
-    def polar3(self, theta):
-        return (250 + 180* math.sin(theta)), (250- 180*math.cos(theta)), (250 + 190* math.sin(theta)), (250- 190*math.cos(theta))
 
     def hour(self):
         hr = int(time.strftime('%H'))
@@ -51,17 +51,10 @@ class clock:
             return hr
 
     def miniute(self):
-        m = int(time.strftime('%M'))
-        return m
+        return int(time.strftime('%M'))
 
     def second(self):
         return int(time.strftime('%S'))
-
-    def refresh(self):
-        clock()
-
-
-
 
 if __name__ == '__main__':
     try:
