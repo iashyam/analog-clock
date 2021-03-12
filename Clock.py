8fb1cbc377e6d63a8ffc88e06ffa60be1bf766b4
Shyam Sunder <70376025+iashyam@users.noreply.github.com>
2021-03-12 08:36:37 +0530

    efficient code
    After I learnt to  clean code

--- Clock.py
+++ Clock.py
@@ -1,63 +1,63 @@
 from tkinter import *
-from PIL import Image, ImageTk
 import time
-import math
+from PIL import ImageTk,Image
+from math import sin, cos, pi
 
-class clock:
+class Clock:
     def __init__(self):
-        self.window= Tk()
-        self.window.title('Clock')
-        self.lc = math.pi/30
-        self.lc_hr = math.pi/6
-        self.lc_hrm = math.pi/(6*60)
-        self.text = "Happy NEw YEar"
 
-        self.img = ImageTk.PhotoImage(Image.open('clock.png'))
+        #defining the constants
+        self.WIDTH = 500
+        self.HEIGHT = 500
+        self.center  = (self.HEIGHT/2, self.WIDTH/2)
+        self.RADIUS = self.WIDTH/2
 
-        self.show_time()
-        self.move()
+        self.lc_hr = pi/6 #deffing the least counts
+        self.lc_min = pi/30
+        self.hc = self.lc_hr/60
 
+        self.window = Tk()
+        self.window.title("Clock")
+        self.window.geometry(f"{self.WIDTH}x{self.HEIGHT}")
+
+        #creatinga a sheet for clock
+        self.canvas = Canvas(self.window, width = self.WIDTH, height =self.HEIGHT)
+        img = ImageTk.PhotoImage(Image.open("clock.png"))
+        self.canvas.create_image(self.center[0], self.center[1], image=img)
+        self.canvas.pack()
+        self.drawHands()
+
+        while True:
+            self.canvas.after(200, self.move)
+            self.canvas.update()
+            
         self.window.mainloop()
 
+
+    def drawHands(self):
+        hr, mn, sec = self.getTime()
+        self.canvas.create_line(self.polar(0,0), self.polar(self.RADIUS/4, self.lc_hr*hr+self.hc*mn), width = 4, tags='hour', fill='green')
+        self.canvas.create_line(self.polar(0,0), self.polar(self.RADIUS//3, self.lc_min*mn), width = 1.5, tags='min', fill='blue')
+        self.canvas.create_line(self.polar(0,0), self.polar(self.RADIUS//2.5, self.lc_min*sec),tags='sec', fill='red')
+
     def move(self):
-        while True:
-            self.window.after(100)
-            self.show_time2()
-            self.window.update()
+        self.canvas.delete('hour', 'min', 'sec')
+        self.drawHands()
 
-    def show_time(self):
-        self.canvas = Canvas(self.window, height=500, width=500,bd=2)
-        self.canvas.pack()
-        self.canvas.create_image(250,250, image=self.img)
-        self.canvas.create_oval(246, 246, 254, 254, fill='black',tags='circle', width=2)
-        self.canvas.create_line(self.polar(100, self.lc * self.second()) ,tags='second',fill='green')
-        self.canvas.create_line(self.polar(70, (self.lc_hr * self.hour()+self.lc_hrm*self.miniute())), width=4,arrow=LAST, tags = 'Hour')
-        self.canvas.create_line(self.polar(95, self.lc * self.miniute()), width=2,arrow=LAST, tags='minute')
-        self.canvas.create_text(250, 90,text=self.text, font='times 20 bold')
-    
-    def show_time2(self):
-        self.canvas.destroy()
-        self.show_time()
-        self.text=time.strftime('%H:%M:%S')
-        
-    def polar(self, radius, theta):
-        return 250, 250, (250 + radius* math.sin(theta)), (250- radius*math.cos(theta))
-
-    def hour(self):
+    def getTime(self):
         hr = int(time.strftime('%H'))
-        if hr>=12:
-            return hr-12
-        else:
-            return hr
-
-    def miniute(self):
-        return int(time.strftime('%M'))
-
-    def second(self):
-        return int(time.strftime('%S'))
+        mn = int(time.strftime('%M'))
+        sec = int(time.strftime('%S'))
+        return hr, mn, sec
+        
+    def polar(self,r, theta):
+        centerX, centerY = self.center
+        x = centerX + r*sin(theta)
+        y = centerY - r*cos(theta)
+        return x, y
 
 if __name__ == '__main__':
     try:
-        clock()
+        Clock()
     except:
         pass
